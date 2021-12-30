74146633c89c1fd7f072b1e9b271ad5b0c99af9e
Andrey Parfenov <asleeppiano@outlook.com>
2021-12-30 21:13:51 +0300

    fixed cache build path, wrapped build script in try catch

--- server/cache/build.js
+++ server/cache/build.js
@@ -2,25 +2,29 @@ const fs = require('fs-extra');
 const path = require('path');
 const { markdownLibrary, getFrontMatter } = require('../services/markdown.js');
 
-const CACHE_FILE = path.resolve('./cache.json');
-const DIR_PATH = path.resolve('../md');
+const CACHE_FILE = path.resolve(__dirname, 'cache.json');
+const DIR_PATH = path.resolve(__dirname, '../md');
 
 function buildCache() {
     if (!fs.existsSync(DIR_PATH)) {
         fs.mkdir(DIR_PATH);
     }
-    const dir = fs.readdirSync(DIR_PATH);
-    let jsonList = [];
-    for(let file of dir) {
-        const content = fs.readFileSync(path.resolve(DIR_PATH, file), 'utf-8');
-        const md = markdownLibrary.render(content);
-        const json = {
-            frontMatter: getFrontMatter(),
-            content: md
+    try {
+        const dir = fs.readdirSync(DIR_PATH);
+        let jsonList = [];
+        for(let file of dir) {
+            const content = fs.readFileSync(path.resolve(DIR_PATH, file), 'utf-8');
+            const md = markdownLibrary.render(content);
+            const json = {
+                frontMatter: getFrontMatter(),
+                content: md
+            }
+            jsonList.push(json);
         }
-        jsonList.push(json);
+        fs.writeFileSync(CACHE_FILE, JSON.stringify(jsonList, null, 2));
+    } catch {
+        // noop
     }
-    fs.writeFileSync(CACHE_FILE, JSON.stringify(jsonList, null, 2));
 }
 
 buildCache();
--- server/models/blog.ts
+++ server/models/blog.ts
@@ -22,12 +22,16 @@ export async function getAllBlogPosts(): Promise<SERVER.Result<BLOG.BlogPost[]>>
 }
 
 export async function getBlogBySlug(slug: string): Promise<SERVER.Result<BLOG.BlogPost>> {
-    const content = await fs.readFile(CACHE_FILE, 'utf-8');
-    const blogPosts = JSON.parse(content);
-    const post = blogPosts.find((post: BLOG.BlogPost) => post.frontMatter.slug === slug);
-    if (post) {
-        return right(post);
-    }
+    try {
+        const content = await fs.readFile(CACHE_FILE, 'utf-8');
+        const blogPosts = JSON.parse(content);
+        const post = blogPosts.find((post: BLOG.BlogPost) => post.frontMatter.slug === slug);
+        if (post) {
+            return right(post);
+        }
 
-    return left({ code: 404 });
+        return left({ code: 404 });
+    } catch (e) {
+        return left({ code: 404 });
+    }
 }
